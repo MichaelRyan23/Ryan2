08e2b8ae8215a996154b15fa15b8003f84a61b43
MichaelRyan23 <michaelryan1823@gmail.com>
2023-10-02 09:09:04 -0500

    Added implementation/class definition

--- Ryan2/main.cpp
+++ Ryan2/main.cpp
@@ -1,1 +1,20 @@
-// Initial committ to see if repository is connected
+/********************************************************************
+*** NAME       : Michael Ryan
+*** CLASS      : CSc 300
+*** ASSIGNMENT : 2
+*** DUE DATE   : 10-5-2023
+*** INSTRUCTOR : GAMRADT
+*********************************************************************
+*** DESCRIPTION : This program 
+*** 
+********************************************************************/             
+#include <iostream>
+// #include "Stack.h"
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
