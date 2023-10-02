106964a40c6ab768ae3f0a62ebea21db6556e813
MichaelRyan23 <michaelryan1823@gmail.com>
2023-10-02 12:03:09 -0500

    Added some basic outline stuff

---
+++ Ryan2/Stack.cpp
@@ -0,0 +1,14 @@
+/********************************************************************
+*** NAME : Michael Ryan
+*** CLASS : CSc 300
+*** ASSIGNMENT : 2
+*** DUE DATE : 10 - 6 - 2023
+*** INSTRUCTOR : GAMRADT
+*********************************************************************
+*** DESCRIPTION : <detailed english description of the abstract data type> 
+*** <including supporting operations> 
+********************************************************************/
+
+#include <iostream>
+#include "Stack.h"
+using namespace std;
---
+++ Ryan2/Stack.h
@@ -0,0 +1,33 @@
+/********************************************************************
+*** NAME : Michael Ryan
+*** CLASS : CSc 300
+*** ASSIGNMENT : 2
+*** DUE DATE : 10 - 6 - 2023
+*** INSTRUCTOR : GAMRADT
+*********************************************************************
+*** DESCRIPTION : <general english description of the abstract data type> 
+*** <including supporting operations>
+********************************************************************/
+
+#ifndef _STACK_H
+#define _STACK_H
+
+#include <iostream>
+using namespace std;
+
+typedef std::string Element;     // making element a string data type
+typedef Element * ElementPtr;
+
+class Stack {
+    public:
+
+
+
+    private:
+
+
+
+
+};
+
+#endif
