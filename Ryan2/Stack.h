70e4bfb10b877c3be1203b0b588a7818e56c96da
MichaelRyan23 <142366498+MichaelRyan23@users.noreply.github.com>
2023-10-05 22:59:47 -0500

    Comment blocks
    Had to manually committ

--- Ryan2/Stack.h
+++ Ryan2/Stack.h
@@ -1,12 +1,13 @@
 /********************************************************************
-*** NAME : Michael Ryan
-*** CLASS : CSc 300
-*** ASSIGNMENT : 2
-*** DUE DATE : 10 - 6 - 2023
-*** INSTRUCTOR : GAMRADT
+*** NAME        : Michael Ryan
+*** CLASS       : CSc 300
+*** ASSIGNMENT  : 2
+*** DUE DATE    : 10 - 6 - 2023
+*** INSTRUCTOR  : GAMRADT
 *********************************************************************
-*** DESCRIPTION : <general english description of the abstract data type> 
-*** <including supporting operations>
+*** DESCRIPTION : A stack is a collection of elements that implements
+*** the First-in-last-out idea where the most recent element added(pushed)
+*** is the first one to be removed (popped). 
 ********************************************************************/
 
 #ifndef _STACK_H
@@ -21,13 +22,83 @@ typedef Element * ElementPtr;
 class Stack {
     public:
 
+/********************************************************************
+*** FUNCTION constructor
+*********************************************************************
+*** DESCRIPTION : Initializes a new stack with a given or default size 
+*** INPUT ARGS : size
+*** OUTPUT ARGS : 
+*** IN/OUT ARGS : 
+*** RETURN : 
+********************************************************************/
         explicit Stack(int size = 2);
+
+/********************************************************************
+*** FUNCTION copy constructor
+*********************************************************************
+*** DESCRIPTION : Creates a copy of an existing stack
+*** INPUT ARGS : old
+*** OUTPUT ARGS : 
+*** IN/OUT ARGS : old
+*** RETURN : 
+********************************************************************/
         Stack(Stack &old);
+
+/********************************************************************
+*** FUNCTION destructor
+*********************************************************************
+*** DESCRIPTION : Cleans up the stack by going popping each element out
+*** and deletes (release) memory.
+*** INPUT ARGS : 
+*** OUTPUT ARGS : 
+*** IN/OUT ARGS : 
+*** RETURN : 
+********************************************************************/
         ~Stack();
 
+/********************************************************************
+*** FUNCTION push
+*********************************************************************
+*** DESCRIPTION : Adds an element to the top of the stack
+*** INPUT ARGS : item
+*** OUTPUT ARGS : 
+*** IN/OUT ARGS : 
+*** RETURN : void
+********************************************************************/
         void push(const Element item);
+
+/********************************************************************
+*** FUNCTION pop
+*********************************************************************
+*** DESCRIPTION : Removes and returns the top element of the stack.
+*** INPUT ARGS : 
+*** OUTPUT ARGS : 
+*** IN/OUT ARGS : 
+*** RETURN : Element
+********************************************************************/
         Element pop();
+
+/********************************************************************
+*** FUNCTION peek
+*********************************************************************
+*** DESCRIPTION : Retrieves top element from stack and pushes it back in
+*** effectively returning top element.
+*** INPUT ARGS : 
+*** OUTPUT ARGS : 
+*** IN/OUT ARGS : 
+*** RETURN : Element
+********************************************************************/
         Element peek();
+
+/********************************************************************
+*** FUNCTION view
+*********************************************************************
+*** DESCRIPTION : Displays the content of the stack from top to bottom.
+*** INPUT ARGS : 
+*** OUTPUT ARGS : 
+*** IN/OUT ARGS : 
+*** RETURN : void
+********************************************************************/
         void view();
 
     private:
